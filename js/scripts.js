ecce504a8ff2c9c2acdabb1dfceb78dc484d0e5c
Dustin Starke and Hingyi Khong <hingyi@>
2018-08-13 11:28:34 -0700

    create html and script

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
-$(document)ready
+$(document).ready(function() {
+  $('#favorites').submit(function(event) {
+
+    var favoriteFoods = $("input#favorite-foods").val();
+    var favoriteColors = $("input#favorite-colors").val();
+    var favoriteIcyF = $("input#favorite-ice-cream").val();
+
+    console.log(favoriteFoods, favoriteColors, favoriteIcyF);
+
+    $(".input-food").text(favoriteFoods);
+    $(".input-colors").text(favoriteColors);
+    $(".input-food").text(favoriteIcyF);
+
+    $("#favorite-things").show();
+
+    event.preventDefault();
+  });
+});
